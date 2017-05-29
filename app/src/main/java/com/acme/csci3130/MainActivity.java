6fc73a7e7047fcc7ce14283b09388be0b04eab9f
Reid <reid.wheeler@dal.ca>
2017-05-29 12:37:35 -0300

    editing stage 2 to match assignment guideline

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -9,23 +9,8 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button b = (Button) findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener()   {
-            public void onClick(View v)  {
-                Validate(v);
-            }
-        });
-    }
-
-    void Validate(View view){
+    int Validate(String password){
         int testsPassed = 0;
-        TextView message = (TextView)findViewById(R.id.helloText);
-        EditText phrase = (EditText) findViewById(R.id.editText);
-        String password = phrase.getText().toString();
 
         if(!(password.toLowerCase().equals("password"))){
             testsPassed++;
@@ -42,11 +27,6 @@ public class MainActivity extends Activity {
         if(password.matches(".*[^A-Za-z0-9].*")){
             testsPassed++;
         }
-        if(testsPassed<5){
-            message.setText("Invalid Password!");
-        }
-        else{
-            message.setText("Valid Password!");
-        }
+        return testsPassed;
     }
 }
